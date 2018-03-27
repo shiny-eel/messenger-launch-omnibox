e43ac79d5010aeb3752e36e8ac36cb0583d24661
Lu Shien Lee <lushienlee@gmail.com>
2018-03-28 02:20:51 +1300

    YES

--- background.js
+++ background.js
@@ -1,51 +1,56 @@
-const fbMessengerURL = "https://www.messenger.com/*"
+const fbMessengerURL = "https://www.messenger.com/*";
+const myKey = "LIST OF PEOPLE";
+
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
-    // chrome.omnibox.onInputStarted.addListener(
-    function(text) {
-        var oldTabID
-        var currentTab
-        // Encode user input for special characters , / ? : @ & = + $ #
-        log("Here");
-
-
-        chrome.tabs.query({
-            "url": fbMessengerURL
-        }, function(tabs) {
-            if (tabs.length > 0) { // Messenger tab already exists
-                var tabID = tabs[0].id;
-                log("Tab ID =" + tabID);
-                // https://developer.chrome.com/extensions/tabs#method-update
-                chrome.tabs.update(tabID, {
-                    "highlighted": true,
-                    "active": true
-                }, function(tab) {
-                    currentTab = tab;
-                    chrome.tabs.update(tab.id, {
-                        "pinned": true
-                    },editMessengerPage(tab.id)
-                );
-
-
-                });
-
-            } else { // Messenger tab needs to be created
-
-                var newURL = 'https://www.messenger.com';
-                chrome.tabs.create({
-                    url: newURL
-                }, function(tab) {
-                    currentTab = tab;
-                    chrome.tabs.update(tab.id, {
-                        "pinned": true
-                    },editMessengerPage(tab.id));
-
-                });
-            }
-        });
-
+    function(text){
+        requestPeople();
     });
+    // chrome.omnibox.onInputStarted.addListener(
 
+
+function goToMessenger(text) {
+    var oldTabID
+    var currentTab
+    // Encode user input for special characters , / ? : @ & = + $ #
+    log("Here");
+
+
+    chrome.tabs.query({
+        "url": fbMessengerURL
+    }, function(tabs) {
+        if (tabs.length > 0) { // Messenger tab already exists
+            var tabID = tabs[0].id;
+            log("Tab ID =" + tabID);
+            // https://developer.chrome.com/extensions/tabs#method-update
+            chrome.tabs.update(tabID, {
+                "highlighted": true,
+                "active": true
+            }, function(tab) {
+                currentTab = tab;
+                chrome.tabs.update(tab.id, {
+                    "pinned": true
+                },editMessengerPage(tab.id)
+            );
+
+
+            });
+
+        } else { // Messenger tab needs to be created
+
+            var newURL = 'https://www.messenger.com';
+            chrome.tabs.create({
+                url: newURL
+            }, function(tab) {
+                currentTab = tab;
+                chrome.tabs.update(tab.id, {
+                    "pinned": true
+                },editMessengerPage(tab.id));
+
+            });
+        }
+    });
+}
 
 function editMessengerPage(tabID) {
     log("Calling the edit function.");
@@ -68,6 +73,27 @@ function log(text) {
     bkg.console.log(text);
 }
 
+function requestPeople() {
+    chrome.storage.sync.get(myKey, function(result) {
+              console.log('Current saved people are: ' + result[myKey]);
+              if (result[myKey].constructor === Array) {
+                  // console.log("IT IS AN ARRAY");
+                  return;
+              } else {
+                  console.log("Failure to read proper settings.");
+              }
+            });
+
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: sender.tab.url});
+  });
 
 
 // chrome.tabs.query({"highlighted": true}, function(tabs) {
--- messengerParasite.js
+++ messengerParasite.js
@@ -1,16 +1,57 @@
 const myKey = "LIST OF PEOPLE";
+const maxSaved = 10;
+var savedPeople = [];
+var currentPerson;
+getCurrentPerson();
 
-function savePerson() {
+function getCurrentPerson() {
+    // Ask the background.js script for the current url
+    chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+      console.log("Response = "+response.farewell);
+      currentPerson = getNameFromURL(response.farewell);
+      console.log("Person's username is: "+currentPerson);
+      loadPeople();
+    });
+}
+
+
+function loadPeople() {
     console.log("Messenger Parasite Active!");
     chrome.storage.sync.get(myKey, function(result) {
-              console.log('Value currently is ' + result[myKey]);
+              console.log('Current saved people are: ' + result[myKey]);
+              if (result[myKey].constructor === Array) {
+                  // console.log("IT IS AN ARRAY");
+                  savedPeople = result[myKey];
+                  if (savedPeople.includes(currentPerson) || savedPeople.length > maxSaved) {
+                      console.log("Already has this person.");
+                  } else {
+                      savedPeople.push(currentPerson);
+                  }
+                  // pushArray(savedPeople, result[myKey]);
+              } else {
+                  console.log("Failure to read proper settings.");
+              }
+              savePeople();
             });
 
-    myValue = [ "tim.sinclair.50", "revolushien"]
-    var items = {};
-    items[myKey] = myValue
-    chrome.storage.sync.set(items, function() {
-          console.log('Value is set to ' + myValue);
-        });
 }
-savePerson();
+
+function savePeople() {
+    var items = {};
+    items[myKey] = savedPeople;
+    // items[myKey] = [];
+    console.log("Updated people: "+items[myKey]);
+    chrome.storage.sync.set(items, function() {
+        console.log('Value is set to ' + items[myKey]);
+    });
+}
+
+function pushArray(arr, arr2) {
+    arr.push.apply(arr, arr2);
+}
+
+function getNameFromURL(url) {
+    // https://www.messenger.com/t/revolushien
+    var newString = url.replace(/https:\/\/www\.messenger\.com\/t\//, "");
+    return newString;
+}
