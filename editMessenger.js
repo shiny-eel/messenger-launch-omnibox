174c8272d9a33d33f094ef0dfb4cf042ff0f7d7c
Lu Shien Lee <lushienlee@gmail.com>
2018-03-31 16:50:26 +1300

    Remove commenting

--- editMessenger.js
+++ editMessenger.js
@@ -91,33 +91,3 @@ function getDesiredURL(callback) {
     });
 
 }
-
-// console.log("Found element " + collection + " length=" + collection.length)
-// var contents = collection.item(0)
-// console.log("Element is: " + contents)
-// contents.select();
-// contents.click();
-// contents.value = "Stephanie";
-// var evt = document.createEvent("HTMLEvents");
-// // adding this created a magic and passes it as if
-// evt.initEvent("change", false, true);
-// contents.dispatchEvent(evt);
-
-// var keyboardEvent = document.createEvent("KeyboardEvent");
-// var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent";
-//
-//
-// keyboardEvent[initMethod](
-//                    "keypress", // event type : keydown, keyup, keypress
-//                     true, // bubbles
-//                     true, // cancelable
-//                     window, // viewArg: should be window
-//                     false, // ctrlKeyArg
-//                     false, // altKeyArg
-//                     false, // shiftKeyArg
-//                     false, // metaKeyArg
-//                     90, // keyCodeArg : unsigned long the virtual key code, else 0
-//                     0 // charCodeArgs : unsigned long the Unicode character associated with the depressed key, else 0
-// );
-// console.log("HERE")
-// document.dispatchEvent(keyboardEvent);
